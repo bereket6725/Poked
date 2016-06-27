246dcc81da6a42e79534976c3fac4f710c9849f7
Bereket <bereket6725@gmail.com>
2016-06-26 22:04:19 -0400

    added some more UI

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,22 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Bereket Ghebremedhin on 6/26/16.
+//  Copyright © 2016 Bereket Ghebremedhin. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var name: String!
+    private var pokedexId: Int!
+
+    
+    
+    init(name: String, pokedexId: Int){
+        self.name       = name
+        self.pokedexId  = pokedexId
+    }
+    
+}
